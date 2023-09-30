0ca95030c2ffc663d7861f18dc4798cc447291d4
xkbmk955 <146537804+xkbmk955@users.noreply.github.com>
2023-09-30 12:54:44 +0200

    init commit

---
+++ zadanie 1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string imie = "Jakub";
+	string numer = "s29744";
+	string szkola = "Polsko-Japonska Akademia techniki komputerowej";
+	cout << imie << endl;
+	cout << numer << endl;
+	cout << szkola<< endl;
+	return 0;
+}
